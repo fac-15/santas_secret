b55e8921ff6a81674c95f9c395d8b358b813443a
Charlie La Fosse <charlielafosse@outlook.com>
2018-12-20 15:30:24 +0000

    posting and getting data

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,26 @@
 const express = require("express");
 const router = express.Router();
+const helpers = require("../views/helpers/index.js");
 
 router.get("/", (req, res) => {
   res.render("home");
 });
 
 router.post("/", (req, res) => {
-  console.log(req.body);
+  console.log(req.body.name);
+  helpers.postData(req.body.name, (err, res) => {
+    if(err){
+      console.log("error in postData: ", err);
+    } else {
+      helpers.getData((error, response) => {
+        if(error){
+          console.log("error in getData: ", error);
+        } else {
+          console.log("getData response: ", response);
+        }
+      })
+    }
+  })
 });
 
 router.get("/results", (req, res) => {
--- src/views/helpers/postData.js
+++ src/views/helpers/postData.js
@@ -2,13 +2,14 @@ const dbConnection = require("../../database/db_connection.js");
 
 const postUserData = (name, cb) => {
   dbConnection.query(
-    "INSERT INTO users (full_name) VALUES ($1) RETURNING id",
+    "INSERT INTO users (full_name) VALUES ($1)",
     [name],
     (err, res) => {
       if (err) {
         console.log("dbConnection error in postData :", err);
+      } else {
+        cb(null);
       }
-      cb(null, res.rows);
     }
   );
 };
