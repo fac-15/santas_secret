c9c90a97fd9ffb2466e6f6f44cadb5bf610741b9
Oliver Smith-Wellnitz <oliversw.artist@gmail.com>
2018-12-20 15:51:07 +0000

    Display current database list on main page

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,33 +2,36 @@ const express = require("express");
 const router = express.Router();
 const helpers = require("../views/helpers/index.js");
 
-router.get("/", (req, res) => {
-  res.render("home");
+router.get("/", (req, response) => {
+  helpers.getData((error, resultGetData) => {
+    if (error) {
+      console.log("error in getData: ", error);
+    } else {
+      console.log("getData response: ", resultGetData);
+      response.render("home", {
+        people: resultGetData
+      });
+    }
+  });
 });
 
-router.post("/", (req, res) => {
+router.post("/", (req, response) => {
   console.log(req.body.name);
-  helpers.postData(req.body.name, (err, res) => {
-    if(err){
+  helpers.postData(req.body.name, (err, resultPostData) => {
+    if (err) {
       console.log("error in postData: ", err);
     } else {
-      helpers.getData((error, response) => {
-        if(error){
-          console.log("error in getData: ", error);
-        } else {
-          console.log("getData response: ", response);
-        }
-      })
+      response.redirect("/");
     }
-  })
+  });
 });
 
-router.get("/results", (req, res) => {
-  res.render("results");
+router.get("/results", (req, response) => {
+  response.render("results");
 });
 
-router.get("/error", (req, res) => {
-  res.render("error");
+router.get("/error", (req, response) => {
+  response.render("error");
 });
 
 module.exports = router;
--- src/views/helpers/postData.js
+++ src/views/helpers/postData.js
@@ -2,7 +2,7 @@ const dbConnection = require("../../database/db_connection.js");
 
 const postUserData = (name, cb) => {
   dbConnection.query(
-    "INSERT INTO users (full_name) VALUES ($1)",
+    "INSERT INTO users (full_name) VALUES ($1) returning id",
     [name],
     (err, res) => {
       if (err) {
