954e5471c277e788e7dfb519f6b006624b5f079e
Lucy <lewright20@gmail.com>
2018-12-20 13:35:53 +0000

    added comments to home and results.hbs and returning full_name in postData helper

--- src/views/helpers/postData.js
+++ src/views/helpers/postData.js
@@ -1,12 +1,12 @@
-const dbConnection = require("../../database/db_connection.js");
+const dbConnection = require('../../database/db_connection.js');
 
 const postUserData = (name, cb) => {
   dbConnection.query(
-    "INSERT INTO users (full_name) VALUES ($1) RETURNING id",
+    'INSERT INTO users (full_name) VALUES ($1) RETURNING full_name',
     [name],
     (err, res) => {
       if (err) {
-        console.log("dbConnection error in postData :", err);
+        console.log('dbConnection error in postData :', err);
       }
       cb(null, res.rows);
     }
