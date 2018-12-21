e39101bf496aea70196c1bd9a476e39e535438d9
Charlie La Fosse <charlielafosse@outlook.com>
2018-12-21 10:02:17 +0000

    change postData, make test pass

--- src/views/helpers/postData.js
+++ src/views/helpers/postData.js
@@ -8,7 +8,11 @@ const postUserData = (name, cb) => {
       if (err) {
         console.log("dbConnection error in postData :", err);
       } else {
-        cb(null);
+        // charlie and Oliver got rid of res.rows in the cb here
+        // for some reason, can't remember why
+        // thought it was maybe to do with db validation
+        // adding it back in makes our postData test pass, yay
+        cb(null, res.rows);
       }
     }
   );
